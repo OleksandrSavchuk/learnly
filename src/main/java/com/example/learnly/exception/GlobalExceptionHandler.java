1b9d0ddb08ac769c5d67e99bbbd09c2a529c54a9
OleksandrSavchuk <sashasavchuk1935@gmail.com>
2026-02-04 19:10:01 +0200

    feat: add new exception handling

--- src/main/java/com/example/learnly/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/learnly/exception/GlobalExceptionHandler.java
@@ -5,8 +5,14 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestControllerAdvice
 @Slf4j
@@ -24,6 +30,38 @@ public class GlobalExceptionHandler {
                 .status(HttpStatus.UNAUTHORIZED)
                 .contentType(MediaType.APPLICATION_JSON)
                 .body(problem);
+    }
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ProblemDetail> handleResourceNotFoundException(ResourceNotFoundException e) {
+        log.warn("404  {}", e.getMessage());
+
+        var problem = ProblemDetail.forStatus(HttpStatus.NOT_FOUND.value());
+        problem.setTitle("Not Found Resource");
+        problem.setDetail(e.getMessage());
+
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(problem);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, List<String>>> handleValidation(
+            MethodArgumentNotValidException e) {
+        log.warn("400  {}", e.getMessage());
+
+        Map<String, List<String>> errors = new HashMap<>();
+
+        e.getBindingResult().getFieldErrors().forEach(error -> {
+            errors
+                    .computeIfAbsent(error.getField(), k -> new ArrayList<>())
+                    .add(error.getDefaultMessage());
+        });
+
+        return ResponseEntity
+                .badRequest()
+                .body(errors);
     }
 
 }
